51b917f93fde12e294a6ab392b0093699a7599fb
Vinicius-SMoraes <vinimoraes611@gmail.com>
2025-04-19 11:58:18 -0300

    first commit

---
+++ src/test/java/TestUser.java
@@ -0,0 +1,32 @@
+// 1 - Bibliotecas
+
+import org.junit.jupiter.api.Test;
+
+
+
+// 2 - Classes
+public abstract class TestUser {
+    //2.1 atributos 
+    static String ct = "application/json"; // Content type da request
+    static String uriPet = "https://petstore.swagger.io/v2/user"; // Base de base + o endpoint do User 
+
+
+    //2.2 funções e metodos 
+    //2.2.1 Funções e métodos comuns / uteis 
+
+    // Função de leitura do Json
+    public static String lerArquivoJson(String aquivoJson){
+        //return new String(Files)
+        return null;
+    }
+
+    // 2.2.2 Métodos de teste
+    @Test
+    public void testPostPet(){
+        // Carregar os dados do arquivo Json do pet
+        // TODO: Criar metodo post
+        // String jsonBody = 
+
+    }
+
+}
